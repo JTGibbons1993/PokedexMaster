9b1c894ab1e4a594ea735021b69e10b06377942a
JTGibbons1993 <jtgibbons1993@gmail.com>
2017-07-08 12:11:12 -0400

    Setup portion of collectionView. Add csv.swift file to parse .csv file into usable Pokemon data.

---
+++ PokedexMaster/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  PokedexMaster
+//
+//  Created by user on 7/7/17.
+//  Copyright © 2017 John. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ PokedexMaster/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokedexMaster
+//
+//  Created by user on 7/6/17.
+//  Copyright © 2017 John. All rights reserved.
+//
+
+import Foundation
