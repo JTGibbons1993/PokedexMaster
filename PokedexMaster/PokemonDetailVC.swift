61a8c441aa4a3f0b5a9b2906cd44797d24366937
JTGibbons1993 <jtgibbons1993@gmail.com>
2017-07-13 14:04:28 -0400

    Add properties to Pokemon class model and setup IBOutlets in DetailVC along with backButton IBAction

--- PokedexMaster/PokemonDetailVC.swift
+++ PokedexMaster/PokemonDetailVC.swift
@@ -12,6 +12,20 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var DefenseLabel: UILabel!
+    @IBOutlet weak var hLabel: UILabel!
+    @IBOutlet weak var wLabel: UILabel!
+    @IBOutlet weak var IDLabel: UILabel!
+    @IBOutlet weak var BaseAtkLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +34,9 @@ class PokemonDetailVC: UIViewController {
 
     }
 
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 
 
 }
